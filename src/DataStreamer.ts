400252e4c183d15fa022f3bddb600155b0a92be1
Perren Wright <pwright2410@gmail.com>
2020-07-31 09:49:23 -0500

    final commit

--- src/DataStreamer.ts
+++ src/DataStreamer.ts
@@ -1,21 +1,21 @@
 export interface Order {
-  price: Number,
-  size: Number,
+  price: Number;
+  size: Number;
 }
 /**
  * The datafeed server returns an array of ServerRespond with 2 stocks.
  * We do not have to manipulate the ServerRespond for the purpose of this task.
  */
 export interface ServerRespond {
-  stock: string,
-  top_bid: Order,
-  top_ask: Order,
-  timestamp: Date,
+  stock: string;
+  top_bid: Order;
+  top_ask: Order;
+  timestamp: Date;
 }
 
 class DataStreamer {
   // The url where datafeed server is listening
-  static API_URL: string = 'http://localhost:8080/query?id=1';
+  static API_URL: string = "http://localhost:8080/query?id=1";
 
   /**
    * Send request to the datafeed server and executes callback function on success
@@ -23,15 +23,15 @@ class DataStreamer {
    */
   static getData(callback: (data: ServerRespond[]) => void): void {
     const request = new XMLHttpRequest();
-    request.open('GET', DataStreamer.API_URL, false);
+    request.open("GET", DataStreamer.API_URL, false);
 
     request.onload = () => {
       if (request.status === 200) {
         callback(JSON.parse(request.responseText));
       } else {
-        alert ('Request failed');
+        alert("Request failed");
       }
-    }
+    };
 
     request.send();
   }
